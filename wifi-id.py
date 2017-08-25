eea73f99b1319dda32c5e51e352c3692ddd87556
Azis R. Pratama <azis.pratama@gmail.com>
2017-08-25 22:18:35 +0700

    Update wifi-id.py
    Sudah work, tinggal bikin daemon yang mampu mendeteksi ping RTO

--- wifi-id.py
+++ wifi-id.py
@@ -1,33 +1,31 @@
 # Script AutoLogin Wifi ID oleh Azis R. Pratama
-# Modifikasi dari script autologin gmail oleh Hongkiat 
+# Modifikasi dari script auto-login gmail oleh Hongkiat (https://github.com/hongkiat/autologinbot) 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-usernameStr = '80615524601'
-passwordStr = '11034897135295'
-#usernameStr = 'pratama'
-#passwordStr = 'rahasia'
+# isikan username (baris 9) & password (baris 10) voucher wifi ID Anda di dalam tanda '...' berikut:
+usernameStr = '80615581892'
+passwordStr = '11034727408335'
 
 browser = webdriver.Chrome()
-browser.get(('file:///home/pratama/wifi-ID/landingPage/Desktop%20Welcome%20Page.html'))
+# Ganti alamat URL wifi ID landing page, sesuai dengan alamat mac Anda: 
+browser.get(('http://welcome2.wifi.id/wifi.id-new/default/?gw_id=WAG-D4-KBU&client_mac=08:3e:8e:27:0a:9d&wlan=SMNSMN00053/TLK-WI329744-0002:@wifi.id&sessionid=0A00FFFF7802E0A7-5997AC79&redirect=http://wifi.id/'))
 
-# isikan username voucher wifi ID Anda
+# baris nomor 16 ke bawah berikut tidak perlu dimodifikasi (biarkan apa adanya): 
 
-username = WebDriverWait(browser, 03).until(
+username = WebDriverWait(browser, 01).until(
     EC.presence_of_element_located((By.ID, 'username_form')))
 
 username.send_keys(usernameStr)
 
-# memberikan sedikit jeda 3 detik
+# memberikan sedikit jeda selama 3 detik
 
-password = WebDriverWait(browser, 03).until(
+password = WebDriverWait(browser, 01).until(
     EC.presence_of_element_located((By.ID, 'password_form')))
 
 password.send_keys(passwordStr)
 
 signInButton = browser.find_element_by_name('Submit')
 signInButton.click()
-
-# Todo List: 
